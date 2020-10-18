0a68016f318ee20f64f6016986fdb9f5ccb76847
snifhex <sachintripathis84@gmail.com>
2020-10-18 17:04:21 +0530

    Initial

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="mail_guesser", 
+    version="0.0.1",
+    author="Sachin Tripathi",
+    author_email="sachintripathi@protonmail.com",
+    description="A Email Guesser",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/snifhex/python-package-development",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
